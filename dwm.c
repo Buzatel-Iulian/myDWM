960f433b744af75b8af2be1fe85aa7fcdb11dae0
Buzatel-Iulian <iulian.buzatel@gmail.com>
2022-10-10 23:15:58 +0300

    Fixed systray overlap & added dwm-restartsig

--- dwm.c
+++ dwm.c
@@ -251,6 +251,8 @@ static void seturgent(Client *c, int urg);
 static void showhide(Client *c);
 static void sigalrm(int unused);
 static void sigchld(int unused);
+static void sighup(int unused);
+static void sigterm(int unused);
 static void spawn(const Arg *arg);
 static Monitor *systraytomon(Monitor *m);
 static void tag(const Arg *arg);
@@ -316,6 +318,7 @@ static void (*handler[LASTEvent]) (XEvent *) = {
 	[UnmapNotify] = unmapnotify
 };
 static Atom wmatom[WMLast], netatom[NetLast], xatom[XLast];
+static int restart = 0;
 static int running = 1;
 static Cur *cursor[CurLast];
 static Clr **scheme;
@@ -1124,7 +1127,7 @@ getsigcmds(int signal)
 int
 getstatus(int width)
 {
-	int i, len, all = width, delimlen = TEXTW(delimiter) - lrpad/2;
+	int i, len, all = width, delimlen = TEXTW(delimiter) - lrpad;
 	char fgcol[8];
 				/* fg		bg */
 	const char *cols[8] = 	{ fgcol, colors[SchemeStatus][ColBg] };
@@ -1143,15 +1146,15 @@ getstatus(int width)
 		/* re-load the scheme with the new colors */
 		scheme[SchemeStatus] = drw_scm_create(drw, cols, 3);
 		drw_setscheme(drw, scheme[SchemeStatus]); /* 're-set' the scheme */
-		len = TEXTW(blockoutput[i]) - lrpad/2;
+		len = TEXTW(blockoutput[i]) - lrpad;
 		all -= len;
-		drw_text(drw, all, 0, len, bh, 0, blockoutput[i], 0);
+		drw_text(drw, all-getsystraywidth(), 0, len, bh, 0, blockoutput[i], 0);
 		/* draw delimiter */
 		if (*delimiter == '\0') /* ignore no delimiter */
 			continue;
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		all -= delimlen;
-		drw_text(drw, all, 0, delimlen, bh, 0, delimiter, 0);
+		drw_text(drw, all-getsystraywidth(), 0, delimlen, bh, 0, delimiter, 0);
 	}
 
 	return stsw = width - all;
@@ -1534,6 +1537,7 @@ propertynotify(XEvent *e)
 void
 quit(const Arg *arg)
 {
+	if(arg->i) restart = 1;
 	running = 0;
 }
 
@@ -1973,6 +1977,9 @@ setup(void)
 	snprintf(envpid, LENGTH(envpid), "%d", getpid());
 	setenv("STATUSBAR", envpid, 1);
 
+	signal(SIGHUP, sighup);
+	signal(SIGTERM, sigterm);
+
 	/* init screen */
 	screen = DefaultScreen(dpy);
 	sw = DisplayWidth(dpy, screen);
@@ -2107,6 +2114,20 @@ void
 sigchld(int unused)
 {
 	while (0 < waitpid(-1, NULL, WNOHANG));
+}
+
+void
+sighup(int unused)
+{
+	Arg a = {.i = 1};
+	quit(&a);
+}
+
+void
+sigterm(int unused)
+{
+	Arg a = {.i = 0};
+	quit(&a);
 }
 
 void
@@ -2781,6 +2802,7 @@ main(int argc, char *argv[])
 #endif /* __OpenBSD__ */
 	scan();
 	run();
+	if(restart) execvp(argv[0], argv);
 	cleanup();
 	XCloseDisplay(dpy);
 	return EXIT_SUCCESS;
